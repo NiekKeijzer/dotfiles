ea25d9c4a42994c40440c7bc145c19cd9fe21f44
Niek Keijzer <info@niekkeijzer.com>
2018-01-23 19:20:42 +0100

    Speedup 'thefuck' alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -29,5 +29,5 @@ alias myip="curl ipecho.net/plain"
 alias kubeui="kubectl proxy --port=8800 & newtab http://localhost:8800/ui"
 
 if command -v thefuck >/dev/null; then
-  eval $(thefuck --alias fuck)
+  alias fuck='TF_CMD=$(TF_ALIAS=fuck PYTHONIOENCODING=utf-8 TF_SHELL_ALIASES=$(alias) thefuck $(fc -ln -1 | tail -n 1)) && eval $TF_CMD ; test -n "$TF_CMD" && print -s $TF_CMD'
 fi
