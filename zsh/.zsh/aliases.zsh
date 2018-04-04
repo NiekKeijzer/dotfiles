cf3a6f9bf7e070c1bfee13fab128ef7691382cb9
Niek Keijzer <niek@qabana.nl>
2018-04-04 17:04:16 +0200

    Add aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -16,27 +16,32 @@ alias sys_update="brew update; sudo softwareupdae -ir"
 alias bye="sudo shutdown -h now"
 alias cya="sudo shutdown -r now"
 
-alias flushdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
+alias flushdns="sudo dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias clean_ds="find . -name '*.DS_Store' -type f -ls -delete"
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
-alias myip="curl https://api.ipify.org"
+
+# Pretty cat
+alias c="pygmentize -O style=borland -f console256 -g"
 
 # ====================================
 # Apps 'n' Stuff
 # ====================================
 
 alias kubeui="kubectl proxy --port=8800 & newtab http://localhost:8800/ui"
+alias publicip="curl https://api.ipify.org"
+alias localip="ipconfig getifaddr en0"
+alias weather="curl -4 http://wttr.in"
 
 if command -v thefuck >/dev/null; then
   alias fuck='TF_CMD=$(TF_ALIAS=fuck PYTHONIOENCODING=utf-8 TF_SHELL_ALIASES=$(alias) thefuck $(fc -ln -1 | tail -n 1)) && eval $TF_CMD ; test -n "$TF_CMD" && print -s $TF_CMD'
 fi
 
-if command -v pyenv >/dev/null; then 
+if command -v pyenv >/dev/null; then
   # Configure PyEnv
   eval "$(pyenv init -)"
-  
-  if command -v pyenv-virtualenv-init >/dev/null; then 
-    # Optionally configure PyEnv virutalenv as well  
-    eval "$(pyenv virtualenv-init -)"; 
+
+  if command -v pyenv-virtualenv-init >/dev/null; then
+    # Optionally configure PyEnv virutalenv as well
+    eval "$(pyenv virtualenv-init -)";
   fi
 fi
