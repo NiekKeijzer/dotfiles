e6a6e3a799efa7cade5fe3b1d5c9d498650d0214
Niek Keijzer <info@niekkeijzer.com>
2018-11-28 13:47:47 +0100

    Add Latex home

--- zsh/.zsh/_path.zsh
+++ zsh/.zsh/_path.zsh
@@ -7,6 +7,7 @@ export PATH="/usr/local/opt/icu4c/sbin:$PATH"
 export GOPATH="/Users/niekkeijzer/go/"
 export PATH="$GOPATH/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
+export PATH="/Library/TeX/texbin:$PATH"
 
 export EDITOR="vim"
 export LC_ALL="en_US.UTF-8"
@@ -15,3 +16,17 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+
+ # Create a JAVA_HOME variable, determined dynamically
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home
+# Add that to the global PATH variable
+export PATH=${JAVA_HOME}/bin:$PATH
+# Set Android_HOME
+export ANDROID_HOME=~/Library/Android/sdk/
+# Add the Android SDK to the ANDROID_HOME variable
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/tools:$PATH
+# Set GRADLE_HOME
+export GRADLE_HOME=/Library/gradle/gradle-3.2
+export PATH=$PATH:$GRADLE_HOME/bin
+# Set Latex home
