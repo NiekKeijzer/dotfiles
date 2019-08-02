2a27d96c7472fcbf09a708b80f00a8b90f92755f
Niek Keijzer <info@niekkeijzer>
2019-08-02 18:37:11 +0200

    Merge conflicts

--- zsh/.zsh/20_aliases.zsh
+++ zsh/.zsh/20_aliases.zsh
@@ -1,2 +1,5 @@
 alias flushdns="sudo killall -HUP mDNSResponder;sudo killall mDNSResponderHelper;sudo dscacheutil -flushcache"
-alias dokku='bash $HOME/.dokku/contrib/dokku_client.sh'
+
+if [ -f "$HOME/.dokku/contrib/dokku_client.sh" ]; then
+  alias dokku='bash $HOME/.dokku/contrib/dokku_client.sh'
+fi
