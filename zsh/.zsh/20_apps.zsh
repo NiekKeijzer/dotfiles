217960bb3b209d308d921478852158f7d4ea44d4
Niek Keijzer <info@niekkeijzer>
2020-08-22 11:57:20 +0200

    feat: various speedups

--- zsh/.zsh/20_apps.zsh
+++ zsh/.zsh/20_apps.zsh
@@ -1,4 +1,14 @@
 if command -v pyenv >/dev/null; then
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
+  zsh-defer eval "$(pyenv init -)"
+  zsh-defer eval "$(pyenv virtualenv-init -)"
 fi
+
+if command -v fuck >/dev/null; then 
+  zsh-defer eval $(fuck () { TF_PYTHONIOENCODING=$PYTHONIOENCODING; export TF_SHELL=zsh; export TF_ALIAS=fuck; TF_SHELL_ALIASES=$(alias); export TF_SHELL_ALIASES; TF_HISTORY="$(fc -ln -10)"; export TF_HISTORY; export PYTHONIOENCODING=utf-8; TF_CMD=$( thefuck THEFUCK_ARGUMENT_PLACEHOLDER $@ ) && eval $TF_CMD; unset TF_HISTORY; export PYTHONIOENCODING=$TF_PYTHONIOENCODING; test -n "$TF_CMD" && print -s $TF_CMD })
+fi
+
+export NVM_DIR="$HOME/.nvm"
+if [ -f "$NVM_DIR/nvm.sh" ]; then 
+  zsh-defer source "$NVM_DIR/nvm.sh"  # This loads nvm
+  zsh-defer source "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+fi  
