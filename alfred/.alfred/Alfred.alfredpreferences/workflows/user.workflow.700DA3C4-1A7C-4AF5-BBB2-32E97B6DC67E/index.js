a1ea4b23eaa59f5ba89131723142ba96343ac984
Niek Keijzer <niek@qabana.nl>
2020-08-26 14:15:23 +0200

    chore: remove alfred and dokku configs

--- alfred/.alfred/Alfred.alfredpreferences/workflows/user.workflow.700DA3C4-1A7C-4AF5-BBB2-32E97B6DC67E/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const parser = require('./lib/parser')
-const request = require('./lib/request')
-const alfy = require('alfy')
-
-let query = parser.parse(alfy.input)
-request.request(query)
-  .then(items => { alfy.output(items) })
-
